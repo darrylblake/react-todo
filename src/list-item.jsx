a91f3545231491a5cde8d4990bfdb174077f99a6
Darryl Blake <darryl@darrylblake.com>
2015-11-20 16:06:02 +1300

    Saving done status.

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -1,15 +1,24 @@
 var React = require('react');
+var Firebase = require('firebase');
+var rootUrl = 'https://sg-react-todo.firebaseio.com/';
 
 module.exports = React.createClass({
   getInitialState: function() {
     return {
-      text: this.props.item.text
+      text: this.props.item.text,
+      done: this.props.item.done
     }
+  },
+  componentWillMount: function() {
+    this.fb = new Firebase(rootUrl + 'items/' + this.props.item.key);
   },
   render: function() {
     return <div className="input-group">
       <span className="input-group-addon">
-        <input type="checkbox" />
+        <input 
+          type="checkbox" 
+          onChange={this.handleDoneChange} 
+          checked={this.state.done} />
       </span>
       <input type="text"
         className="form-control"
@@ -20,5 +29,10 @@ module.exports = React.createClass({
         </button>
       </span>
     </div>
+  },
+  handleDoneChange: function(event) {
+    var update = {done: event.target.checked};
+    this.setState(update);
+    this.fb.update(update);
   }
 });
